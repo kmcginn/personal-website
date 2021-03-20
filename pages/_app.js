739527c09a4737eeedf3ddac0558a54af05d4924
Kevin McGinn <kmcginn14@gmail.com>
2021-03-20 11:04:10 -0700

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+// fix for FontAwesomeIcons disappearing when favicon is set
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
