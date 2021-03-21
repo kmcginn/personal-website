267c8241e256c1d11e43e970bda51c273ce4a490
Kevin McGinn <kmcginn14@gmail.com>
2021-03-20 20:51:31 -0700

    add rel values to ensure external link is safe

--- components/socialicon.js
+++ components/socialicon.js
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SocialIcon({iconId, iconName, link}) {
     return (
-        <a href={link} target="_blank">
+        <a href={link} target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon title={iconName} icon={['fab', iconId]} size="3x"/>
         </a>
     )
