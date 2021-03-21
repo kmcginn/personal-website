ce15cfb18f77dbb0a55223c4041b05a79fcbe6ef
Kevin McGinn <kmcginn14@gmail.com>
2021-03-21 10:27:50 -0700

    format all files with prettier

--- components/socialicon.js
+++ components/socialicon.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function SocialIcon({iconId, iconName, link}) {
-    return (
-        <a href={link} target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon title={iconName} icon={['fab', iconId]} size="3x"/>
-        </a>
-    )
+export default function SocialIcon({ iconId, iconName, link }) {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <FontAwesomeIcon title={iconName} icon={["fab", iconId]} size="3x" />
+    </a>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
-import Head from 'next/head'
-import SocialIcon from '../components/socialicon'
+import Head from "next/head";
+import SocialIcon from "../components/socialicon";
 
 export default function Home() {
   return (
     <div className="min-h-screen py-2 flex flex-col justify-center items-center">
       <Head>
         <title>Hello!</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="px-20 flex-1 flex flex-col justify-center items-center text-center">
@@ -24,11 +38,23 @@ export default function Home() {
       <footer className="w-full h-28 border-t-2 border-solid border-blue-100 flex justify-center items-center">
         <div className="grid grid-rows-1 grid-cols-3 gap-x-3">
           <span className="sr-only">Social media links</span>
-          <SocialIcon iconId="github-square" iconName="GitHub" link="https://github.com/kmcginn"/>
-          <SocialIcon iconId="dev" iconName="Dev Community" link="https://dev.to/kmcginn"/>
-          <SocialIcon iconId="twitter-square" iconName="Twitter" link="https://twitter.com/kmcginn14"/>
+          <SocialIcon
+            iconId="github-square"
+            iconName="GitHub"
+            link="https://github.com/kmcginn"
+          />
+          <SocialIcon
+            iconId="dev"
+            iconName="Dev Community"
+            link="https://dev.to/kmcginn"
+          />
+          <SocialIcon
+            iconId="twitter-square"
+            iconName="Twitter"
+            link="https://twitter.com/kmcginn14"
+          />
         </div>
       </footer>
     </div>
-  )
+  );
 }
