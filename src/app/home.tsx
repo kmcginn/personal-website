520b9bc8a57882301250d7ab792db5369ec6a500
Kevin McGinn <kmcginn14@gmail.com>
2023-09-30 23:37:19 -0700

    fix site metadata

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,33 +1,11 @@
 'use client'
 
-import Head from "next/head";
 import SocialIcon from "../components/socialicon";
 import {faLinkedin, faGithubSquare, faDev} from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
     <div className="min-h-screen py-2 flex flex-col justify-center items-center">
-      <Head>
-        <title>Hello!</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
 
       <main className="px-20 flex-1 flex flex-col justify-center items-center text-center">
         <h1 className="text-3xl sm:text-5xl md:text-7xl leading-relaxed sm:leading-relaxed md:leading-relaxed font-bold">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,26 @@
 import "../styles/globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hello!",
+  icons: [
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png", sizes: "180x180" },
+    { rel: "icon", type: "image/png", url: "/favicon-32x32.png", sizes: "32x32"},
+    { rel: "icon", type: "image/png", url: "/favicon-16x16.png", sizes: "16x16"}
+  ],
+  manifest: "/site.webmanifest"
+};
 
 export default function RootLayout({
-    // Layouts must accept a children prop.
-    // This will be populated with nested layouts or pages
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
